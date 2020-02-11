3bf5f35ffb04767b2a97618addffb77dae98c83e
Thomas Rago <thomasrago@Thomass-iMac.fios-router.home>
2020-02-11 14:58:01 -0500

    update menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage/Homepage";
 import "./App.css";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
