e69a47bf4c6e69d95c8e647a840aa0b1334bb85c
Thomas Rago <thomasrago@Thomass-iMac.fios-router.home>
2020-02-11 13:52:40 -0500

    Finished styling menu components

---
+++ src/pages/Homepage/Homepage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Directory from "../../components/Directory/Directory";
+import "./Homepage.styles.scss";
+
+const Homepage = () => {
+  return (
+    <div className="Homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default Homepage;
