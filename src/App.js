cb78a3155bd01a8b434a7bf9c2a116f9e7a4fc1b
Thomas Rago <thomasrago@Thomass-iMac.fios-router.home>
2020-02-16 20:24:25 -0500

    Added header component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage/Homepage";
 import ShopPage from "./pages/Shop/ShopPage";
+import Header from "./components/Header/Header";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
