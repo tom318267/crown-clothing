a241eb551cdf548584ce1e0c023a024a493a70ef
Thomas Rago <thomasrago@Thomass-iMac.fios-router.home>
2020-04-23 15:23:14 -0400

    add shop data to firestore

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,4 +1,5 @@
 import CartActionTypes from "./cart.types";
+import swal from "sweetalert";
 
 export const toggleCartHidden = () => {
   return {
@@ -21,6 +22,7 @@ export const removeItem = item => {
 };
 
 export const clearItemFromCart = item => {
+  swal("Deleted", `You deleted this item from your cart!`, "success");
   return {
     type: CartActionTypes.CLEAR_ITEM_FROM_CART,
     payload: item
