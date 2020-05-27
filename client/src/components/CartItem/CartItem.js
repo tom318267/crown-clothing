1da73f962beb4edd652c8d665c3b75f0840b625c
Thomas Rago <thomasrago@Thomass-iMac.fios-router.home>
2020-05-27 15:04:23 -0400

    add delete butotn to cartdropdown

--- client/src/components/CartItem/CartItem.js
+++ client/src/components/CartItem/CartItem.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { connect } from "react-redux";
+import { clearItemFromCart } from "../../redux/cart/cart.actions";
 import "./CartItem.scss";
 
-const CartItem = ({ item: { imageUrl, price, name, quantity } }) => {
+const CartItem = ({
+  item: { imageUrl, price, name, quantity },
+  clearItem,
+  item,
+}) => {
   return (
     <div className="cart-item">
       <img src={imageUrl} alt="item" />
@@ -10,9 +16,19 @@ const CartItem = ({ item: { imageUrl, price, name, quantity } }) => {
         <span className="price">
           {quantity} x ${price}
         </span>
+        <div
+          style={{ fontWeight: "bold", cursor: "pointer" }}
+          onClick={() => clearItem(item)}
+        >
+          <i class="fas fa-trash"></i>
+        </div>
       </div>
     </div>
   );
 };
 
-export default CartItem;
+const mapDispatchToProps = (dispatch) => ({
+  clearItem: (item) => dispatch(clearItemFromCart(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CartItem);
